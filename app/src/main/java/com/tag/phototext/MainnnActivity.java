e83a557e936b55a1a7d108cee9248d56fd452458
MILON <milon.hexit@gmail.com>
2017-06-08 16:20:18 +0600

    Gallery

--- app/src/main/java/com/tag/phototext/MainnnActivity.java
+++ app/src/main/java/com/tag/phototext/MainnnActivity.java
@@ -1,20 +1,35 @@
 package com.tag.phototext;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
 
 import com.example.takeimage.R;
+import com.kyo.imagecrop.CropUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class MainnnActivity extends AppCompatActivity {
+
+    private int SELECT_FILE = 1, REQUEST_CAMERA = 0;
+    Intent GalIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +38,31 @@ public class MainnnActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final GridView gv= (GridView) findViewById(R.id.gv);
+        final GridView gv = (GridView) findViewById(R.id.gv);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_Gal:
+                        GalleryOpen();
+                        break;
+
+                    case R.id.action_Histotry:
+                        break;
+
+                    case R.id.action_Cam:
+                        break;
+                }
+                return true;
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gv.setAdapter(new CustomAdapter(MainnnActivity.this,getPDFs()));
+                gv.setAdapter(new CustomAdapter(MainnnActivity.this, getPDFs()));
 
             }
         });
@@ -38,25 +71,22 @@ public class MainnnActivity extends AppCompatActivity {
     private ArrayList<PDFDoc> getPDFs()
 
     {
-        ArrayList<PDFDoc> pdfDocs=new ArrayList<>();
+        ArrayList<PDFDoc> pdfDocs = new ArrayList<>();
         //TARGET FOLDER
-        File downloadsFolder= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 
         PDFDoc pdfDoc;
 
-        if(downloadsFolder.exists())
-        {
+        if (downloadsFolder.exists()) {
             //GET ALL FILES IN DOWNLOAD FOLDER
-            File[] files=downloadsFolder.listFiles();
+            File[] files = downloadsFolder.listFiles();
 
             //LOOP THRU THOSE FILES GETTING NAME AND URI
-            for (int i=0;i<files.length;i++)
-            {
-                File file=files[i];
+            for (int i = 0; i < files.length; i++) {
+                File file = files[i];
 
-                if(file.getPath().endsWith("pdf"))
-                {
-                    pdfDoc=new PDFDoc();
+                if (file.getPath().endsWith("pdf")) {
+                    pdfDoc = new PDFDoc();
                     pdfDoc.setName(file.getName());
                     pdfDoc.setPath(file.getAbsolutePath());
 
@@ -69,5 +99,65 @@ public class MainnnActivity extends AppCompatActivity {
         return pdfDocs;
     }
 
+
+    private void GalleryOpen() {
+        GalIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(GalIntent, "Select Image from Gallery"), SELECT_FILE);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        if (data != null) {
+            try {
+                Log.w("no", "Null");
+                TextClass.sUri = data.getData();
+                TextClass.sbitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+                startCropper(SELECT_FILE, data);
+                //CropImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+//
+//		ivImage.setImageBitmap(bitmap);
+//        Intent intent = new Intent(getApplicationContext(), Ocr2.class);
+//        startActivity(intent);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+        }
+    }
+
+    private void startCropper(int requestCode, Intent data) {
+        Uri uri = null;
+        if (requestCode == REQUEST_CAMERA) {
+            uri = TextClass.sUri;
+        } else if (data != null && data.getData() != null) {
+            uri = TextClass.sUri;
+        } else {
+            return;
+        }
+        Intent intent = new Intent(this, CropActivity.class);
+        intent.setData(uri);
+        intent.putExtra("outputX", CropUtils.dip2px(this, 300));
+        intent.putExtra("outputY", CropUtils.dip2px(this, 150));
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        startActivity(intent);
+    }
+
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
 
 }
