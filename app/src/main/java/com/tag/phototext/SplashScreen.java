d5fe004c46fdccbffee1a1e021287d6df3db5cf1
MILON <milon.hexit@gmail.com>
2017-07-25 16:08:13 +0600

    auto permission completed

--- app/src/main/java/com/tag/phototext/SplashScreen.java
+++ app/src/main/java/com/tag/phototext/SplashScreen.java
@@ -1,7 +1,10 @@
 package com.tag.phototext;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -17,6 +21,8 @@ public class SplashScreen extends AppCompatActivity {
     private static final long SPLASH_TIME_OUT = 1500;
     private SharedPreferences mSharedPreferences;
     String flag;
+    int REQUEST_CAMERA = 101;
+    boolean isPermitted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,57 @@ public class SplashScreen extends AppCompatActivity {
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         flag = mSharedPreferences.getString("camera", "1");
         Glide.with(this).load(R.drawable.splashscreen2).into(imageView);
+        checkRunTimePermission();
+    }
+
+
+    private void checkRunTimePermission() {
+        String[] permissionArrays = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissionArrays, 11111);
+        } else {
+            // if already permition granted
+            // PUT YOUR ACTION (Like Open cemara etc..)
+            next();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean openDialogOnce = true;
+        if (requestCode == 11111) {
+            for (int i = 0; i < grantResults.length; i++) {
+                String permission = permissions[i];
+
+                isPermitted = grantResults[i] == PackageManager.PERMISSION_GRANTED;
+
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    // user rejected the permission
+                    boolean showRationale = false;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                        showRationale = shouldShowRequestPermissionRationale(permission);
+                    }
+                    if (!showRationale) {
+                        //execute when 'never Ask Again' tick and permission dialog not show
+                    } else {
+                        if (openDialogOnce) {
+                            Toast.makeText(this, "Not", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+
+            if (isPermitted)
+                next();
+//                if (isPermissionFromGallery)
+//                    openGalleryFragment();
+        }
+    }
+
+    public void next() {
         new Handler().postDelayed(new Runnable() {
 
             /*
@@ -50,5 +107,6 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+
     }
 }
